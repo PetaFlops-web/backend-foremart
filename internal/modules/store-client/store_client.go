2e2e225649c814c1d2a302dd0c740a3c1c9dd262
Artt Vincii <otong123321@gmail.com>
2026-07-29 23:20:01 +0700

    feat(store): add public store client methods

--- internal/modules/store-client/store_client.go
+++ internal/modules/store-client/store_client.go
@@ -1,6 +1,16 @@
 package store_client
 
+import "context"
+
+type StoreDTO struct {
+	ID        string
+	UserID    string
+	StoreName string
+	CreatedAt int64
+	UpdatedAt int64
+}
 
 type StoreClient interface {
-	
+	GetStoreByID(ctx context.Context, storeID string) (*StoreDTO, error)
+	GetStoreByUserID(ctx context.Context, userID string) (*StoreDTO, error)
 }
