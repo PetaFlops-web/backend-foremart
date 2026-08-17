ba3da793299da2866986a2d0ba231052c355c6b6
Putra Rizky <putra@smart-commerce.dev>
2026-08-17 23:37:18 +0700

    -

--- internal/modules/restock/src/model/restock_request.go
+++ internal/modules/restock/src/model/restock_request.go
@@ -1,9 +1,11 @@
 package model
 
+import "time"
+
 type GenerateRestockPredictionRequest struct {
-	StoreID            string `json:"store_id" validate:"required"`
-	ProductID          string `json:"product_id,omitempty"`
-	ForecastDate       string `json:"forecast_date,omitempty"`
-	HistoryDays        int    `json:"history_days,omitempty"`
-	ForecastWindowDays int    `json:"forecast_window_days,omitempty"`
+	StoreID            string     `json:"store_id" validate:"required"`
+	ProductID          string     `json:"product_id,omitempty"`
+	ForecastDate       *time.Time `json:"forecast_date,omitempty"`
+	HistoryDays        int        `json:"history_days,omitempty"`
+	ForecastWindowDays int        `json:"forecast_window_days,omitempty"`
 }
